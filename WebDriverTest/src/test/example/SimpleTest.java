9d3acd797adf54214222a408f1a5104d6c7d585e
Sonal <chatterjeesonal@ymail.com>
2018-05-09 14:25:03 +0530

    Initial commit

---
+++ WebDriverTest/src/test/example/SimpleTest.java
@@ -0,0 +1,11 @@
+package example;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+	 @Test
+	 public void test() {
+		 System.out.printLn("I am in SimpleTest");
+		 
+	 }
+
+}
---
+++ WebDriverTest/src/test/java/qaeg/QAT2.java
@@ -0,0 +1,11 @@
+package qaeg;
+
+import org.testng.annotations.Test;
+
+public class QAT2 {
+	 @Test
+	 public void test() {
+		 System.out.println("I am in src-test-java");
+		 
+	 }
+}

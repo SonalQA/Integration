d10ec93e699829fe393a79d40bdc68fb3d60fd35
Sonal <sonal.chatterjee@entytle.com>
2018-05-14 12:29:42 +0530

    This test runs in chrome

--- WebDriverTest/src/test/java/qaeg/QAT2.java
+++ WebDriverTest/src/test/java/qaeg/QAT2.java
@@ -1,5 +1,10 @@
 package qaeg;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class QAT2 {
@@ -7,5 +12,26 @@ public class QAT2 {
 	 public void test() {
 		 System.out.println("I am in src-test-java");
 		 
+	 }
+	 
+	 @Test
+	 public void verify() {
+		 System.setProperty("webdriver.chrome.driver", "C:\\SelDrivers\\chromedriver.exe");
+
+
+		  //  WebDriver driver = new FirefoxDriver();
+		   WebDriver driver = new ChromeDriver();
+
+	       driver.get("http://blazemeter.com");
+
+	       WebElement title = driver.findElement(By.xpath("//title"));
+
+	       System.out.println("********************************************************");
+
+	       System.out.println("*" + title.getAttribute("text") + "*");
+
+	       System.out.println("********************************************************");
+
+	       driver.quit();
 	 }
 }

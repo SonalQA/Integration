0e997c57da6bd08a40f8fe023fce0388a6508138
Sonal <sonal.chatterjee@entytle.com>
2018-05-15 16:27:52 +0530

    This is chrome again

--- WebDriverTest/src/test/java/qaeg/QAT2.java
+++ WebDriverTest/src/test/java/qaeg/QAT2.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class QAT2 {
@@ -19,9 +19,12 @@ public class QAT2 {
 		 // System.setProperty("webdriver.chrome.driver", "C:\\SelDrivers\\chromedriver.exe");
 
 
-		 FirefoxDriver driver = new FirefoxDriver();
-		  // WebDriver driver = new ChromeDriver();
-
+		 // WebDriver driver = new FirefoxDriver();
+		  WebDriver driver = new ChromeDriver();
+/*		 FirefoxOptions options = new FirefoxOptions();
+		 options.addArgument("--headless");
+		 WebDriver driver = new FirefoxDriver(options);*/
+		 
 	       driver.get("http://blazemeter.com");
 
 	       WebElement title = driver.findElement(By.xpath("//title"));

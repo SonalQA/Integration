2c838a3ad16f1acc3f44b305bae2df957eb82771
Sonal <sonal.chatterjee@entytle.com>
2018-05-16 10:40:42 +0530

    forEC2

--- WebDriverTest/src/test/java/qaeg/QAT2.java
+++ WebDriverTest/src/test/java/qaeg/QAT2.java
@@ -10,6 +10,9 @@ import org.testng.annotations.Test;
 public class QAT2 {
 	 @Test
 	 public void test() {
+		 WebDriver driver = new FirefoxDriver();
+	       driver.get("http://blazemeter.com");
+
 		 System.out.println("I am in src-test-java");
 		 
 	 }
